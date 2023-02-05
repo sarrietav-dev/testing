4535a89a93096ba1fb2365631ac8831ec3cc1fc0
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-02-05 12:54:39 -0500

    Add tests for the value service

--- src/app/services/value.service.spec.ts
+++ src/app/services/value.service.spec.ts
@@ -1,16 +1,36 @@
-import { TestBed } from '@angular/core/testing';
-
 import { ValueService } from './value.service';
 
-describe('ValueService', () => {
+fdescribe('ValueService', () => {
   let service: ValueService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ValueService);
+    service = new ValueService();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('tests for accessing value', () => {
+    it('should return the value', () => {
+      expect(service.getValue()).toBe('A value');
+    });
+
+    it('should change the value', () => {
+      service.setValue('New value');
+      expect(service.getValue()).toBe('New value');
+    });
+
+    it('should return the value from a promise using async', async () => {
+      const result = await service.getPromiseValue();
+      expect(result).toBe('A value');
+    });
+
+    it('should return the value with observable', (done) => {
+      service.getObservableValue().subscribe((result) => {
+        expect(result).toBe('A value');
+        done();
+      });
+    });
   });
 });
--- src/app/services/value.service.ts
+++ src/app/services/value.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ValueService {
+  value = 'A value';
 
-  constructor() { }
+  constructor() {}
+
+  getValue() {
+    return this.value;
+  }
+
+  setValue(newValue: string) {
+    this.value = newValue;
+  }
+
+  getPromiseValue() {
+    return Promise.resolve(this.value);
+  }
+
+  getObservableValue() {
+    return of(this.value);
+  }
 }
