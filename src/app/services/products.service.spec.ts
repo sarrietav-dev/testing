40b224b1ca4394da2aaa3e19e1ec4c09cbce3bea
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-02-06 09:09:11 -0500

    Add test to products service

--- src/app/services/products.service.spec.ts
+++ src/app/services/products.service.spec.ts
@@ -1,16 +1,59 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { environment } from 'src/environments/environment';
+import { Product } from '../models/product.model';
 
 import { ProductsService } from './products.service';
 
 describe('ProductsService', () => {
   let service: ProductsService;
+  let httpController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [ProductsService],
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(ProductsService);
+    httpController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('tests for getAllSimple()', () => {
+    it('should return an array of products', (done) => {
+      const mockData: Product[] = [
+        {
+          id: '1',
+          title: 'Title 1',
+          description: 'Description 1',
+          price: 100,
+          category: {
+            id: 1,
+            name: 'Category 1',
+          },
+          images: [
+            'https://picsum.photos/200/300',
+            'https://picsum.photos/200/300',
+          ],
+          taxes: 19,
+        },
+      ];
+
+      service.getAllSimple().subscribe((products) => {
+        expect(products).toEqual(mockData);
+        done();
+      });
+
+      const url = `${environment.API_URL}/api/v1/products`;
+      const req = httpController.expectOne(url);
+      req.flush(mockData);
+      httpController.verify();
+    });
   });
 });
