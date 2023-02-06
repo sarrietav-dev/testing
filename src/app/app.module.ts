d142d69c3448d239034d09106aa08638d36b20dc
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-02-06 07:26:00 -0500

    Use HttpClient and ReactiveForms modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './components/products/products.component';
 import { PicoPreviewComponent } from './components/pico-preview/pico-preview.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductsComponent,
-    PicoPreviewComponent
-  ],
+  declarations: [AppComponent, ProductsComponent, PicoPreviewComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
