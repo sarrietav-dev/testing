a764c277df3dfde81d56d4abe9a7ac352519272c
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-02-05 20:58:47 -0500

    Define routes for components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PicoPreviewComponent } from './components/pico-preview/pico-preview.component';
+import { ProductsComponent } from './components/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'products',
+    component: ProductsComponent,
+  },
+  {
+    path: 'pico-preview',
+    component: PicoPreviewComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
