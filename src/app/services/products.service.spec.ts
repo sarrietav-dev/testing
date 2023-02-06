a7ac6a3fba79ce11b2e214375d00e88eec48fd90
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-02-06 15:49:23 -0500

    Test product taxes field

--- src/app/services/products.service.spec.ts
+++ src/app/services/products.service.spec.ts
@@ -4,7 +4,10 @@ import {
 } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { environment } from 'src/environments/environment';
-import { generateManyProducts } from '../models/product.mock';
+import {
+  generateManyProducts,
+  generateOneProduct,
+} from '../models/product.mock';
 import { Product } from '../models/product.model';
 
 import { ProductsService } from './products.service';
@@ -33,6 +36,31 @@ describe('ProductsService', () => {
       service.getAllSimple().subscribe((products) => {
         expect(products.length).toBe(10);
         expect(products).toEqual(mockData);
+        done();
+      });
+
+      const url = `${environment.API_URL}/api/v1/products`;
+      const req = httpController.expectOne(url);
+      req.flush(mockData);
+      httpController.verify();
+    });
+
+    it('should return product list with taxes', (done) => {
+      const mockData: Product[] = [
+        {
+          ...generateOneProduct(),
+          price: 100,
+        },
+        {
+          ...generateOneProduct(),
+          price: 200,
+        },
+      ];
+
+      service.getAllSimple().subscribe((products) => {
+        expect(products.length).toBe(mockData.length);
+        expect(products[0].taxes).toEqual(19);
+        expect(products[1].taxes).toEqual(31);
         done();
       });
 
