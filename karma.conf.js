3ca094d932886cbfd319dc8b2d5eb129fa719c2f
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-02-07 23:03:29 -0500

    Add custom launcher for karma

--- karma.conf.js
+++ karma.conf.js
@@ -32,6 +32,12 @@ module.exports = function (config) {
     },
     reporters: ["mocha"],
     browsers: ["ChromeHeadless"],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: "ChromeHeadless",
+        flags: ["--no-sandbox"]
+      }
+    },
     restartOnFileChange: true,
   });
 };
